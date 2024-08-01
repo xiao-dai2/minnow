d59691c9e05b3f358f0623d156f6254373c1ba60
xiao <1684576118@qq.com>
2024-08-01 17:46:03 +0800

    finish step1

--- src/wrapping_integers.cc
+++ src/wrapping_integers.cc
@@ -1,19 +1,28 @@
 #include "wrapping_integers.hh"
-
+#include<vector>
+#include<cmath>
+#include<iostream>
 using namespace std;
 
 Wrap32 Wrap32::wrap( uint64_t n, Wrap32 zero_point )
 {
-  // Your code here.
-  (void)n;
-  (void)zero_point;
-  return Wrap32 { 0 };
+  return Wrap32{uint32_t((zero_point.raw_value_+(n<<32>>32))<<32>>32)};
 }
 
 uint64_t Wrap32::unwrap( Wrap32 zero_point, uint64_t checkpoint ) const
 {
-  // Your code here.
-  (void)zero_point;
-  (void)checkpoint;
-  return {};
+  zero_point.raw_value_ = (((long long)(raw_value_)-(long long)zero_point.raw_value_)+(1UL<<32))<<32>>32;
+  cout<<zero_point.raw_value_<<endl;
+  uint64_t tmp = checkpoint>>32;
+  cout<<tmp<<endl;
+  uint64_t a=zero_point.raw_value_+(tmp<<32),b = zero_point.raw_value_+((tmp+1)<<32);
+  uint64_t c = zero_point.raw_value_;
+  if(tmp>=1)
+  c = zero_point.raw_value_+((tmp-1)<<32);
+  uint64_t ret=a;
+  if(max(ret,checkpoint)-min(ret,checkpoint)>max(b,checkpoint)-min(b,checkpoint)) 
+    ret=b;
+  if(max(ret,checkpoint)-min(ret,checkpoint)>max(c,checkpoint)-min(c,checkpoint)) 
+    ret=c;
+  return ret;
 }
